dcce8db2c948230512290984c50ea2d28f1bbe43
greatkene <ihevuemeg@gmail.com>
2022-07-15 00:14:33 +0100

    Folder Structure

---
+++ app/constants/index.js
@@ -0,0 +1,12 @@
+import constants from './constants'
+import theme, {COLORS, SIZES, FONTS} from './constants/theme'
+import dummyData from './dummyData'
+
+export {
+    constants,
+    theme,
+    COLORS,
+    SIZES,
+    FONTS,
+    dummyData
+}
